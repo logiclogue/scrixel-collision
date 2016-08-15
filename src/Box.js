1e7e4b0b12747ddd428bbdb783d1091dd3d8ac84
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-15 20:48:54 +0100

    Adds Box class and testing

---
+++ src/Box.js
@@ -0,0 +1,14 @@
+function Box(width, height) {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+}
+
+(function (static_, proto_) {
+
+    
+
+}(Box, Box.prototype));
+
+module.exports = Box;
---
+++ test/Box-test.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert;
+var Box = require('../src/Box');
+
+
+describe('Box', function () {
+    var boxA;
+    var boxB;
+
+    describe('#constructor', function () {
+        boxA = new Box(3, 2);
+        boxB = new Box(2, 2);
+
+        it('should set correct x and y values', function () {
+            assert.equal(boxA.x, 0);
+            assert.equal(boxA.y, 0);
+        });
+
+        it('should set correct width and height', function () {
+            boxA.width = 3;
+            boxA.height = 2;
+        });
+    });
+});
