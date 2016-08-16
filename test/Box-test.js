5dd9ea470b08d58ecef806b618fc838dc7bd3ce0
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-16 10:25:44 +0100

    Updates Box class test

--- test/Box-test.js
+++ test/Box-test.js
@@ -13,11 +13,15 @@ describe('Box', function () {
         it('should set correct x and y values', function () {
             assert.equal(boxA.x, 0);
             assert.equal(boxA.y, 0);
+            assert.equal(boxB.x, 0);
+            assert.equal(boxB.y, 0);
         });
 
         it('should set correct width and height', function () {
             boxA.width = 3;
             boxA.height = 2;
+            boxB.width = 2;
+            boxB.height = 2;
         });
     });
 });
