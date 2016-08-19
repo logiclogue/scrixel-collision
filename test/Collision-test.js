8af2d76d14d7cc4895bf51981a0dc2b0b71de3dc
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-19 13:25:02 +0100

    Adds tests for isTouching()

--- test/Collision-test.js
+++ test/Collision-test.js
@@ -1,37 +1,76 @@
 var assert = require('chai').assert;
-var Group = require('../src/Group');
 var Box = require('../src/Box');
+var Group = require('../src/Group');
 var Collision = require('../src/Collision');
 
 
 describe('Collision', function () {
-    var boxA = new Box(2, 4);
-    var boxB = new Box(1, 1);
+    var boxA = new Box(3, 4);
+    var boxB = new Box(2, 2);
     var groupA = new Group();
     var groupB = new Group();
+    var collision = new Collision(boxA, boxB);
 
-    groupA.addBox(boxA);
-    groupB.addBox(boxB);
-
-    describe('#constructor(Group, Group)', function () {
-        it('shouldn\'t throw an error if groups are passed', function () {
-            var collision = Collision.bind(undefined, groupA, groupB);
+    describe('#constructor(Box, Box)', function () {
+        it('shouldn\'t throw an error if boxes are passed', function () {
+            var collision = Collision.bind(undefined, boxA, boxB);
 
             assert.doesNotThrow(collision, ReferenceError);
         });
 
-        it('should throw error if boxes passed', function () {
-            var collision = Collision.bind(undefined, boxA, boxB);
+        it('should throw error if groups passed', function () {
+            var groupA = new Group();
+            var groupB = new Group();
+            var collision = Collision.bind(undefined, groupA, groupB);
 
             assert.throws(collision, ReferenceError);
         });
 
-        it('should throw error if groups aren\'t passed', function () {
+        it('should throw error if boxes aren\'t passed', function () {
             assert.throws(Collision, ReferenceError);
         });
     });
 
-    describe('#test()', function () {
-        
+    describe('#isTouching()', function () {
+        it('should return true when boxes are touching', function () {
+            boxA.x = -1;
+            boxA.y = -1;
+
+            assert.equal(collision.isTouching(), true);
+        });
+
+        it('should return false when boxes are outside', function () {
+            boxA.x = -10;
+            boxA.y = 10;
+
+            assert.equal(collision.isTouching(), false);
+        });
+
+        it('should return true when boxes are inside', function () {
+            boxA.x = 1;
+            boxA.y = 1;
+
+            assert.equal(collision.isTouching(), true);
+        });
+    });
+    
+    describe('#isNorth()', function () {
+
+    });
+
+    describe('#isWest()', function () {
+
+    });
+    
+    describe('#isSouth()', function () {
+
+    });
+
+    describe('#isEast()', function () {
+
+    });
+
+    describe('#isInside()', function () {
+
     });
 });
