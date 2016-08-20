f09d1f075fe780eab0224659e263c952d7290c49
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-20 16:58:01 +0100

    Passing for x setter

--- src/Box.js
+++ src/Box.js
@@ -1,14 +1,50 @@
 function Box(width, height) {
-    this.x = 0;
-    this.y = 0;
+    this._x = 0;
+    this._y = 0;
+    this.prevX = null;
+    this.prevY = null;
     this.width = width;
     this.height = height;
 }
 
-(function (static_, proto_) {
+(function (static_, proto_, def) {
 
-    
+    def('x', {
+        set: function (x) {
+            var oldX = this._x;
 
-}(Box, Box.prototype));
+            this._x = x;
+
+            if (this.prevX === null) {
+                this.prevX = x;
+            }
+            else {
+                this.prevX = oldX;
+            }
+        },
+        get: function () {
+            return this._x;
+        }
+    });
+
+    def('y', {
+        set: function (y) {
+            var oldY = this._y;
+
+            this._y = y;
+
+            if (this.prevY === null) {
+                this.prevY = y;
+            }
+            else {
+                this.prevY = oldY
+            }
+        },
+        get: function () {
+            return this._y;
+        }
+    });
+
+}(Box, Box.prototype, Object.defineProperty.bind({}, Box.prototype)));
 
 module.exports = Box;
