2e65802d09c7ead413efe09e71720e20f71c19e0
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-20 15:57:14 +0100

    Passing isInside() test

--- src/Collision.js
+++ src/Collision.js
@@ -31,7 +31,21 @@ function Collision(boxA, boxB) {
      * Returns true if boxB is inside boxA.
      */
     proto_.isInside = function () {
+        var boxAmax = this._getMax(this.boxA);
+        var boxBmax = this._getMax(this.boxB);
 
+        var isNorth = this.boxA.y >= this.boxB.y;
+        var isEast = boxAmax[0] <= boxBmax[0];
+        var isSouth = boxAmax[1] <= boxBmax[1];
+        var isWest = this.boxA.x >= this.boxB.x;
+
+        console.log(isNorth, isEast, isSouth, isWest);
+
+        if (isNorth && isEast && isSouth && isWest) {
+            return true;
+        }
+
+        return false;
     };
 
 
@@ -77,6 +91,17 @@ function Collision(boxA, boxB) {
         }
 
         return false;
+    };
+
+    /*
+     * Gets maximum coords of a box.
+     * Both x and y.
+     */
+    proto_._getMax = function (box) {
+        return [
+            box.x + box.width,
+            box.y + box.height
+        ];
     };
 
 }(Collision, Collision.prototype));
