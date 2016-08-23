b4dbff97c5d71684b5ae92ffccc07c9e146414e7
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-23 22:42:10 +0100

    Adds index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var CollisionManager = require('./src/CollisionManager');
+var Collision = require('./src/Collision');
+var Group = require('./src/Group');
+var Box = require('./src/Box');
+
+
+module.exports = {
+    CollisionManager: CollisionManager,
+    Collision: Collision,
+    Group: Group,
+    Box: Box
+}
